25ec0885673df4c84c62430784a1fb3271f3e23f
Maciej Dzik <dzikdrums@gmail.com>
2019-09-04 11:23:33 +0200

    add booking table basic functionality

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,60 @@
-import {settings, select} from './settings.js';
+import {settings, select, classNames} from './settings.js';
 import Product from './components/Product.js';
-import Cart from './components/cart.js';
+import Cart from './components/Cart.js';
+import Booking from './components/Booking.js';
 
 const app = {
+  initBooking: function() {
+    const thisApp = this;
+
+    const bookingWidgetElem = document.querySelector(select.containerOf.booking);
+    thisApp.bookingWidget = new Booking(bookingWidgetElem);
+
+  },
+  initPages: function() {
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+    const idFromHash = window.location.hash.replace('#/', '');
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for (let page of thisApp.pages) {
+      if (page.id == idFromHash) {
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+
+    thisApp.activatePage(pageMatchingHash);
+
+    for (let link of thisApp.navLinks)
+      link.addEventListener('click', function(event) {
+        const clickedElement = this;
+        event.preventDefault();
+
+        let id = clickedElement.getAttribute('href').replace('#', '');
+        thisApp.activatePage(id);
+        window.location.hash = '#/' + id;
+      });
+  },
+  activatePage: function(pageId) {
+    const thisApp = this;
+
+    // add class 'active'  to matching pages, remove from non-matching
+    for (let page of thisApp.pages) {
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+    // add class 'active'  to matching links, remove from non-matching
+    for (let link of thisApp.navLinks) {
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+
+  },
   initCart: function() {
     const thisApp = this;
     const cartElem = document.querySelector(select.containerOf.cart);
@@ -39,8 +91,10 @@ const app = {
   init: function() {
     const thisApp = this;
 
+    thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
+    thisApp.initBooking();
   },
 };
 
